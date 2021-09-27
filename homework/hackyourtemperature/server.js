b5a9870c154fb789b3e6052e8ae253534ffa51ed
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-09-27 22:07:03 +0200

    Fixed property cityName

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello from backend to frontend!</h1>");
 });
 app.post("/weather", (req, res) => {
-  const cityName = req.body;
-  res.send(cityName);
+  const { cityName } = req.body;
+  res.send(req.body);
 });
 
 const PORT = process.env.PORT || 3000;
