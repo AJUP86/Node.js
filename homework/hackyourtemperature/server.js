f1e33c6b14932671924cd6beb54d7bdf00c7f96b
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-09-28 00:54:25 +0200

    Requested weather api in hackyourtemperature

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
-
+import fetch from "node-fetch";
+import { keys } from "./sources/keys.js";
 const app = express();
 
 app.use(express.json());
@@ -7,9 +8,26 @@ app.use(urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("<h1>Hello from backend to frontend!</h1>");
 });
-app.post("/weather", (req, res) => {
-  const { cityName } = req.body;
-  res.send(req.body);
+app.post("/:cityName", async (req, res) => {
+  try {
+    const key = keys.appKey;
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.cityName}&&appid=${key}`;
+    const apiData = await fetch(api);
+    if (apiData.ok) {
+      const jsonData = await apiData.json();
+      const kelvin = jsonData.main.temp;
+      const celsius = kelvin - 273.15;
+      const fixedCelsius = celsius.toFixed(2);
+      res.send({
+        cityName: jsonData.name,
+        temperature: `${fixedCelsius} C`,
+      });
+    } else {
+      throw new Error("City not found");
+    }
+  } catch (er) {
+    res.send(er.message);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
---
+++ homework/hackyourtemperature/sources/keys.js
@@ -0,0 +1,1 @@
+export const keys = { appKey: `f78cfd04ad104699c602dcf9dd610a2c` };
